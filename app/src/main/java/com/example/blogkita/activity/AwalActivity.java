897f97804aef4d76f353afa885037e3a3280735e
hilmiwicak <hilmiwicaksonop@gmail.com>
2020-12-27 22:55:48 +0700

    awal activity updated2

--- app/src/main/java/com/example/blogkita/activity/AwalActivity.java
+++ app/src/main/java/com/example/blogkita/activity/AwalActivity.java
@@ -16,28 +16,29 @@ public class AwalActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_awal);
 
-        Button pengunjung = (Button)findViewById(R.id.btn_pengunjung);
-        Button register = (Button)findViewById(R.id.btn_daftar);
-        Button login = (Button)findViewById(R.id.btn_login);
+        Button pengunjung = (Button) findViewById(R.id.btn_pengunjung);
+        Button register = (Button) findViewById(R.id.btn_daftar);
+        Button login = (Button) findViewById(R.id.btn_masuk);
 
-        pengunjung.setOnClickListener(AwalActivity.this);
-        register.setOnClickListener(AwalActivity.this);
-        login.setOnClickListener(AwalActivity.this);
+        pengunjung.setOnClickListener(this);
+        register.setOnClickListener(this);
+        login.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.btn_pengunjung:
-                Intent moveIntent = new Intent(AwalActivity.this, MainActivity.class);
+                Intent moveIntent = new Intent(this, MainActivity.class);
                 startActivity(moveIntent);
                 break;
             case R.id.btn_masuk:
-                moveIntent = new Intent(AwalActivity.this, LoginActivity.class);
+                moveIntent = new Intent(this, LoginActivity.class);
                 startActivity(moveIntent);
                 break;
             case R.id.btn_daftar:
-                moveIntent = new Intent(AwalActivity.this, RegisterActivity.class);
+                moveIntent = new Intent(this, RegisterActivity.class);
                 startActivity(moveIntent);
                 break;
         }
