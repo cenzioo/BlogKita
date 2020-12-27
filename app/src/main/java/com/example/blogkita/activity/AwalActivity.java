506bbe6e21235894f802e372a0ec105f6f0d0137
hilmiwicak <hilmiwicaksonop@gmail.com>
2020-12-27 22:34:50 +0700

    awal activity updated1

--- app/src/main/java/com/example/blogkita/activity/AwalActivity.java
+++ app/src/main/java/com/example/blogkita/activity/AwalActivity.java
@@ -14,7 +14,7 @@ public class AwalActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_awal);
 
         Button pengunjung = (Button)findViewById(R.id.btn_pengunjung);
         Button register = (Button)findViewById(R.id.btn_daftar);
