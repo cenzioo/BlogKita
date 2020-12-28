16fe3d5978a6c4a483777dd13e2542a1efbdd6db
hilmiwicak <hilmiwicaksonop@gmail.com>
2020-12-28 15:17:33 +0700

    nambah DetailPostActivity sm activity_detail_post

---
+++ app/src/main/java/com/example/blogkita/activity/DetailPostActivity.java
@@ -0,0 +1,19 @@
+package com.example.blogkita.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.blogkita.R;
+
+public class DetailPostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_post);
+
+
+
+    }
+}
